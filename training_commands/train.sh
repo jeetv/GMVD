d8b518d650995d59b5d643a720e102d789d08925
Jeet Vora <jeet.vora2010@gmail.com>
2021-09-14 15:37:38 +0530

    Update train.sh

--- training_commands/train.sh
+++ training_commands/train.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
 ##################### Wildtrack Dataset training #####################
 ## Traditional training
-python main.py --avgpool --earlystop 8
+python main.py --avgpool
 
 ## Changing Camera Config
-python main.py --avgpool --cam_set --train_cam 2 4 5 6 --test_cam 2 4 5 6 --earlystop 8
-python main.py --avgpool --cam_set --train_cam 1 3 5 7 --test_cam 1 3 5 7 --earlystop 7
+python main.py --avgpool --cam_set --train_cam 2 4 5 6 --test_cam 2 4 5 6
+python main.py --avgpool --cam_set --train_cam 1 3 5 7 --test_cam 1 3 5 7
 
 ##################### MultiviewX Dataset training #####################
 ## Traditional training
-python main.py -d multiviewx --avgpool --earlystop 9
+python main.py -d multiviewx --avgpool
 
 ## Changing Camera Config
-python main.py -d multiviewx --avgpool --cam_set --train_cam 1 3 4 --test_cam 1 3 4 --earlystop 8
-python main.py -d multiviewx --avgpool --cam_set --train_cam 2 5 6 --test_cam 2 5 6 --earlystop 8
+python main.py -d multiviewx --avgpool --cam_set --train_cam 1 3 4 --test_cam 1 3 4
+python main.py -d multiviewx --avgpool --cam_set --train_cam 2 5 6 --test_cam 2 5 6
